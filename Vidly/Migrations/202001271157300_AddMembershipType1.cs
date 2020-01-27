d3295aa25b2da00a8772385221618c32007ba3bb
gerasimos <gerikool1@gmail.com>
2020-01-27 16:26:43 +0200

    03_SeedingDatabase

---
+++ Vidly/Migrations/202001271157300_AddMembershipType1.cs
@@ -0,0 +1,32 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddMembershipType1 : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Customers", "MembershipTypeId", "dbo.MembershipTypes");
+            DropIndex("dbo.Customers", new[] { "MembershipTypeId" });
+            DropTable("dbo.MembershipTypes");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.MembershipTypes",
+                c => new
+                    {
+                        Id = c.Byte(nullable: false),
+                        SignUpFee = c.Short(nullable: false),
+                        DurationInMonths = c.Byte(nullable: false),
+                        DiscountRate = c.Byte(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateIndex("dbo.Customers", "MembershipTypeId");
+            AddForeignKey("dbo.Customers", "MembershipTypeId", "dbo.MembershipTypes", "Id", cascadeDelete: true);
+        }
+    }
+}
